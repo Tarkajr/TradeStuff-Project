c9464ee6c388f7fc3fea84fae0bbd57075d52cb2
Tarkajr <mattedwards92@gmail.com>
2019-04-29 20:11:54 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import { View, Text, FlatList, StyleSheet, ActivityIndicator } from "react-native";
+import { ListItem } from "react-native-elements";
+import Item from "./Components/Item";
+import PopUp from "./Components/PopUp";
+
+export default class App extends Component {
+
+  constructor(props) {
+     super(props);
+     this.state = {
+       loading: false,
+       data:[],
+       counter: 0,
+       error: null
+     };
+   }
+
+  componentDidMount() {
+  const urlparams = '?sortBy=3&rows=6'
+  this._getStuff(urlparams);
+  }
+
+  _getStuff = async (params) => {
+  const url = `https://api.tradestuff.com/stuff`;
+  this.setState({ loading: true });
+  try {
+    let resp = await fetch(url + params)
+    let response = await resp.json()
+    this.setState({
+      data: [...this.state.data, ...response.data ],
+      error: response.error || null,
+      loading: false,
+      counter: this.state.counter + 6
+    });
+  } catch (error) {
+    this.setState({error: error});
+  }
+  }
+
+  handleReload = () => {
+    const newurlparams = '?sortBy=3&rows=6&offset=' + this.state.counter
+    this._getStuff(newurlparams)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <FlatList
+        onEndReached={this.handleReload}
+        onEndReachedThreshold={0}
+        numColumns = {2}
+        data={this.state.data}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+        <ListItem
+        leftAvatar={{
+        title: item.name,
+        source: {uri: item.media[0].url},
+        rounded: true,
+        containerStyle: styles.avatar,
+        onPress: () => alert(item.name + item.description + " Value: $" + item.value + " Minimum Trade Value: $" + item.min_trade_val + " Category ID: " + item.category_id + " Condition ID: " + item.condition_id)
+        }}
+        />
+        )}
+        />
+        <ActivityIndicator
+        size="large"
+        color="#0000ff"
+        animating = {this.state.loading}
+        style = {styles.loading}
+        />
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  avatar: {
+    margin: 5,
+    height: 200,
+    width: 150,
+  },
+  loading: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
---
+++ Components/PopUp.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { ListItem } from "react-native-elements";
+
+export default class PopUp extends Component {
+
+  constructor(props) {
+     super(props);
+   }
+
+   render() {
+     return (
+      <View>
+      </View>
+     );
+    }
+}
