babb7ff43b4551f10b3151018998c00b5c71abc6
Tarkajr <mattedwards92@gmail.com>
2019-05-08 00:48:04 -0700

    resolve error

--- src/actions.js
+++ src/actions.js
@@ -21,15 +21,15 @@ function fetchStuffSuccess(json) {
   return {
     type: FETCH_STUFF_SUCCESS,
     items: json.data.children.map(child => child.data),
-    receivedAt: Date.now()
   }
 }
 
 export function _getStuffAsync(urlparams) {
+  console.log('test')
   return dispatch => {
     dispatch(fetchStuffRequest())
     return fetch(URL + urlparams)
-      .then(response => response.json())
+      .then(response => response.json(), console.log(response))
       .then(json => dispatch(fetchStuffSuccess(json), incrementCounter()))
   }
 }
